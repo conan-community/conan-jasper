73155ea4a62aa20226895a4a7b6333771401c48a
Uilian Ries <uilianries@gmail.com>
2019-04-03 15:54:05 -0300

    Apply Conan Conventions
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,7 +1,8 @@
-from conan.packager import ConanMultiPackager
+# -*- coding: utf-8 -*-
+from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="jasper:shared")
+    builder.add_common_builds(pure_c=True)
     builder.run()
--- conanfile.py
+++ conanfile.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+import os
 from conans import ConanFile, CMake, tools
 
 
@@ -5,15 +7,19 @@ class JasperConan(ConanFile):
     name = "jasper"
     version = "2.0.14"
     license = "JasPer License Version 2.0"
-    homepage = "http://www.ece.uvic.ca/~frodo/jasper/"
+    homepage = "https://github.com/mdadams/jasper"
     url = "https://github.com/conan-community/conan-jasper"
+    author = "Conan Community"
+    topics = ("conan", "jasper", "tool-kit", "coding")
     description = "JasPer Image Processing/Coding Tool Kit"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False],
                "fPIC": [True, False],
                "jpegturbo": [True, False]}
-    default_options = "shared=False", "fPIC=True", "jpegturbo=False"
+    default_options = {'shared': False, 'fPIC': True, 'jpegturbo': False}
     generators = "cmake"
+    exports = "LICENSE"
+    _source_subfolder = "source_subfolder"
 
     def requirements(self):
         if self.options.jpegturbo:
@@ -29,33 +35,40 @@ class JasperConan(ConanFile):
         del self.settings.compiler.libcxx
 
     def source(self):
-        tools.get("https://github.com/mdadams/jasper/archive/version-%s.zip" % self.version)
-        tools.replace_in_file("jasper-version-%s/CMakeLists.txt" % self.version,
+        sha256 = "05de26be16cf7356f73e8202c0bf4e05597e2c2698a7100d8603253d65f8a790"
+        tools.get("{}/archive/version-{}.zip".format(self.homepage, self.version), sha256=sha256)
+        extracted_dir = "{}-version-{}".format(self.name, self.version)
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["JAS_ENABLE_DOC"] = False
+        cmake.definitions["JAS_ENABLE_PROGRAMS"] = False
+        cmake.definitions["JAS_ENABLE_SHARED"] = self.options.shared
+        cmake.definitions["JAS_LIBJPEG_REQUIRED"] = "REQUIRED"
+        cmake.definitions["JAS_ENABLE_OPENGL"] = False
+        cmake.configure(source_folder=self._source_subfolder)
+        return cmake
+
+    def _patch_cmake(self):
+        cmake_file = os.path.join(self._source_subfolder, "CMakeLists.txt")
+        tools.replace_in_file(cmake_file,
                               "project(JasPer LANGUAGES C)",
                               """project(JasPer LANGUAGES C)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()
-""")
+                                 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+                                 conan_basic_setup()""")
 
     def build(self):
-        cmake = CMake(self)
-        cmake.definitions["JAS_ENABLE_DOC"] = "False"
-        cmake.definitions["JAS_ENABLE_PROGRAMS"] = "False"
-        cmake.definitions["JAS_ENABLE_SHARED"] = "True" if self.options.shared else "False"
-        cmake.definitions["JAS_LIBJPEG_REQUIRED"] = "REQUIRED"
-        cmake.definitions["JAS_ENABLE_OPENGL"] = "False"
-        cmake.configure(source_folder="jasper-version-%s" % self.version)
+        self._patch_cmake()
+        cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include", src="jasper-version-%s/src/libjasper/include" % self.version)
-        self.copy("*.h", dst="include", src="src/libjasper/include")
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so*", dst="lib", keep_path=False, symlinks=True)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
-        self.copy("LICENSE", src="jasper-version-%s" % self.version)
+        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
+        cmake = self._configure_cmake()
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ["jasper"]
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append("m")
